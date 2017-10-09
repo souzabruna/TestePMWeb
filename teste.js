c8d8bcebe82b00dcf5798a68090cab40af738f1b
souzabruna <brunaelisangelasouza@gmail.com>
2017-10-08 22:21:21 -0300

    first commit

---
+++ teste.js
@@ -0,0 +1,165 @@
+var navTop = document.getElementById('navTop');
+    navTop.style.background = '#e74c3c';
+
+var caminhoPasso1 = document.getElementById('caminhoPasso1');
+    caminhoPasso1.style.color = '#e74c3c';
+
+var caminhoPasso2 = document.getElementById('caminhoPasso2');
+    caminhoPasso2.style.color = '#e74c3c';
+
+var caminhoPasso3 = document.getElementById('caminhoPasso3');
+    caminhoPasso3.style.color = '#fff';
+
+var listagemHoteisContent = document.getElementById('listagemHoteisContent');
+    listagemHoteisContent.style.display = 'flex';
+    listagemHoteisContent.style.flexDirection = 'row';
+
+var itemHotel =  document.querySelector(".itemHotel");
+    itemHotel.style.color = '#e74c3c';
+    itemHotel.style.display = 'flex';
+    itemHotel.style.flexDirection = 'column';
+    itemHotel.style.width = '220px';
+
+var itemHotel2 =  document.querySelector(".itemHotel:nth-child(2)");
+    itemHotel2.style.color = '#e74c3c';
+    itemHotel2.style.display = 'flex';
+    itemHotel2.style.flexDirection = 'column';
+    itemHotel2.style.width = '220px';
+
+var itemHotel3 =  document.querySelector(".itemHotel:nth-child(3)");
+    itemHotel3.style.color = '#e74c3c';
+    itemHotel3.style.display = 'flex';
+    itemHotel3.style.flexDirection = 'column';
+    itemHotel3.style.width = '220px';
+
+var itemHotelContent =  document.querySelector(".itemHotelContent");
+    itemHotelContent.style.marginLeft ='0';
+    itemHotelContent.style.height = '270px';
+
+var itemHotelContent2 =  document.querySelector(".itemHotelContent:nth-child(2)");
+    itemHotelContent2.style.marginLeft ='0';
+    itemHotelContent2.style.height = '270px';
+
+var itemHotelContent3 =  document.querySelector(".itemHotelContent:nth-child(3)");
+    itemHotelContent3.style.marginLeft ='0';
+    itemHotelContent3.style.height = '270px';
+
+var iconDestino = document.querySelector(".icon-destino");
+    iconDestino.style.color = '#e74c3c';
+
+var calendar = document.querySelector(".icon-calendario");
+    calendar.style.color = '#e74c3c';
+
+var people = document.querySelector(".icon-busca-pessoas");
+    people.style.color = '#e74c3c';
+
+var baby = document.querySelector(".icon-busca-baby");
+    baby.style.color = '#e74c3c';
+
+var label = document.querySelector(".busca-field-label");
+    label.style.color = '#e74c3c';
+
+var labelPeriodo = document.querySelector("#busca-field-periodo span");
+    labelPeriodo.style.color = '#e74c3c';
+
+var labelBusca = document.querySelector(".busca-field:nth-child(2) span");
+    labelBusca.style.color = '#e74c3c';
+
+var buscaContainer = document.querySelector("#busca-container-adultos-criancas span");
+    buscaContainer.style.color = '#e74c3c';
+
+var buscaCriancaSpan = document.querySelector("#busca-field-criancas span");
+    buscaCriancaSpan.style.color = '#e74c3c';
+
+var iconAdulto = document.querySelector(".icon-busca-1-adulto");
+    iconAdulto.style.color = '#e74c3c';
+
+var icon2Adultos = document.querySelector(".icon-busca-2-adultos");
+    icon2Adultos.style.color = '#e74c3c';
+
+var iconChildren = document.querySelector(".icon-busca-1-crianca");
+    iconChildren.style.color = '#e74c3c';
+
+var iconChildren2 = document.querySelector(".icon-busca-2-criancas");
+    iconChildren2.style.color = '#e74c3c';
+
+var btnReserva = document.querySelector(".itemBtnEfetuarReserva");
+    btnReserva.style.background = '#e74c3c';
+    btnReserva.style.marginLeft ='100px';
+    btnReserva.style.marginTop = '-35px';
+
+var mapIcon = document.querySelector(".icon-endereco");
+    mapIcon.style.color = '#e74c3c';
+
+var calculaData = document.querySelector(".celulaData");
+    calculaData.style.borderRadius = '100%';
+
+var btnBusca = document.getElementById('busca-btn');
+    btnBusca.style.background = '#e74c3c';
+
+var promoCode = document.getElementById('busca-promocode-label');
+    promoCode.style.color = '#e74c3c';
+
+var menuFlutuanteDestino = document.getElementById('busca-flutuante-icon-destino');
+    menuFlutuanteDestino.style.color = '#e74c3c';
+
+var calendarFlutuanteIcon = document.getElementById('busca-flutuante-icon-periodo');
+    calendarFlutuanteIcon.style.color = '#e74c3c';
+
+var adultoFlutuanteIcon = document.getElementById('busca-flutuante-icon-adultos');
+    adultoFlutuanteIcon.style.color = '#e74c3c';
+
+var criancaFlutuanteIcon = document.getElementById('busca-flutuante-icon-criancas');
+    criancaFlutuanteIcon.style.color = '#e74c3c';
+
+var reserveBlock =  document.querySelector(".blocoReserve");
+    reserveBlock.style.width = '100%';
+    reserveBlock.style.display = 'flex';
+    reserveBlock.flexDirection = 'row';
+    reserveBlock.flexWrap = 'wrap';
+
+var formasPagamento =  document.querySelector(".itemVarDescricaoFormasPagamento");
+    formasPagamento.style.width = '100%';
+
+var acomodacoes =  document.querySelector(".itemBtnMaisAcomodacoes");
+    acomodacoes.style.width = '100%';
+
+var descontoPercentual = document.querySelector(".itemVarPercentualDesconto");
+    descontoPercentual.style.display = 'none';
+
+var selectAcomodacoes  = document.querySelector(".itemBtnSelecionarAcomodacao");
+    selectAcomodacoes.style.height = '45px';
+    selectAcomodacoes.style.marginLeft = '0';
+
+var valorSemDesconto = document.querySelector(".itemVarValorSemDesconto");
+    valorSemDesconto.style.width = '30%';
+    valorSemDesconto.style.marginTop = '-15px';
+
+var valueTotal = document.querySelector(".itemVarValorFinal");
+    valueTotal.style.marginLeft = '-65px';
+    valueTotal.style.width = '35%';
+
+var acomodacao1Pessoa = document.querySelector(".icoAcomodacaoUmaPessoa");
+    acomodacao1Pessoa.style.top = '-15px';
+    acomodacao1Pessoa.style.left = '-15px';
+
+var infoAcomodacao = document.querySelector(".infoAcomodacao");
+    infoAcomodacao.style.display = 'flex';
+    infoAcomodacao.style.flexDirection = 'row';
+    infoAcomodacao.style.flexWrap = 'wrap';
+
+var maisPessoas = document.querySelector(".hoteis-ico-mais-pessoas");
+    maisPessoas.style.left = '20px';
+
+var nameAcomodacao = document.querySelector(".itemVarNomeAcomodacao");
+    nameAcomodacao.style.marginTop = '-20px';
+    nameAcomodacao.style.marginLeft = '60px';
+
+var descriptionAcomodacao = document.querySelector(".itemVarDescricaoAcomodacao");
+    descriptionAcomodacao.style.marginLeft = '50px';
+
+var nameHotel = document.querySelector(".itemVarNomeHotel");
+    nameHotel.style.fontSize = '20px';
+
+var htmlModal = '<div style="position: absolute;">  <div style="background-color:rgba(0, 0, 0, 0.6); z-index: 29; height: 100vh; top: 0; left: 0; position: fixed;width: 100vw;"></div><div style="border-radius:5px;overflow:hidden;width:850px;height:500px;z-index:30;position:fixed;left:20%;top:10%;background-color:white;"> </div></div>';
+    document.body.innerHTML += htmlModal;
