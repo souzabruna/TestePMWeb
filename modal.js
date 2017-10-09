b293804d9c44f403c0b13f01abe6e30c2d48bb6f
souzabruna <brunaelisangelasouza@gmail.com>
2017-10-08 22:31:14 -0300

    inserir modal js

---
+++ modal.js
@@ -0,0 +1,2 @@
+var htmlModal = '<div style="position: absolute;">  <div style="background-color:rgba(0, 0, 0, 0.6); z-index: 29; height: 100vh; top: 0; left: 0; position: fixed;width: 100vw;"></div><div style="border-radius:5px;overflow:hidden;width:850px;height:500px;z-index:30;position:fixed;left:20%;top:10%;background-color:white;"> </div></div>';
+    document.body.innerHTML += htmlModal;
