3c559ff589d3f0e8e1dcedec24509ca02b60c059
souzabruna <brunaelisangelasouza@gmail.com>
2017-10-09 00:33:09 -0300

    removed code not used

--- teste.js
+++ teste.js
@@ -163,6 +163,3 @@ var descriptionAcomodacao = document.querySelector(".itemVarDescricaoAcomodacao"
 
 var nameHotel = document.querySelector(".itemVarNomeHotel");
     nameHotel.style.fontSize = '20px';
-
-var htmlModal = '<div style="position: absolute;">  <div style="background-color:rgba(0, 0, 0, 0.6); z-index: 29; height: 100vh; top: 0; left: 0; position: fixed;width: 100vw;"></div><div style="border-radius:5px;overflow:hidden;width:850px;height:500px;z-index:30;position:fixed;left:20%;top:10%;background-color:white;"> </div></div>';
-    document.body.innerHTML += htmlModal;
